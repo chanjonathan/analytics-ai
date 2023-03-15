83d659fe741b71c094f830b6c39a9a1949d4e06c
jonathan-chan-alida <jonathan-chan-alida@github.com>
2023-03-15 10:46:03 -0700

    Add voice to text basic

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React from 'react';
 import { FaMicrophone } from 'react-icons/fa';
 import Graph from './Graph';
 import './App.css';
+import { useWhisper } from '@chengsokdara/use-whisper'
+
+const JONATHAN_OPENAI_KEY = "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";
 
 function App() {
+
+  const {
+    recording,
+    speaking,
+    transcribing,
+    transcript,
+    pauseRecording,
+    startRecording,
+    stopRecording,
+  } = useWhisper({
+    apiKey: JONATHAN_OPENAI_KEY
+  })
+
+  
+
   return (
     <div className="container">
       <div className="input-container">
-        <input type="text" className="input" />
+        <input type="text" className="input" value={transcript.text}/>
+        <button onClick={startRecording}>Record</button>
+        <button onClick={stopRecording}>Stop</button>
         <FaMicrophone className="microphone-icon" />
       </div>
       <Graph />
